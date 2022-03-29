7a8f8153078547e6b45e0544a529f1089f37f52b
Michael <michael.spengler@sap.com>
2022-03-29 11:51:39 +0200

    prepare unit test demo - will be a.o. triggered by gh-action

---
+++ src/gh-action-proxy/transform-and-update.spec.ts
@@ -0,0 +1,10 @@
+import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
+import { GHActionProxy } from "./transform-and-update.ts"
+
+
+Deno.test("should transform markdown to json", async () => {
+
+    // GHActionProxy.transform()
+    assertEquals(1, 1)
+
+})
